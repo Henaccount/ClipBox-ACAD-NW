04d01e97fc698439158f90901a43639609947350
Henaccount <36333524+Henaccount@users.noreply.github.com>
2021-01-31 16:59:36 +0100

    Add files via upload
    handling units, writing multiline batch for model "splitting" (for BIM 360)

--- ClipBoxNw/Program.cs
+++ ClipBoxNw/Program.cs
@@ -18,7 +18,180 @@ namespace ClipBoxNw
     public class Program
     {
 
-        public static void ShowType(ObjectId id)
+        [CommandMethod("ClipBoxNwWriteBatchOnly", CommandFlags.UsePickSet)]
+        public static void ClipBoxNwWriteBatchOnly()
+        { doclipboxnw("ClipBoxNwWriteBatchOnly"); }
+
+        [CommandMethod("ClipBoxNwReset", CommandFlags.UsePickSet)]
+        public static void ClipBoxNwReset()
+        { doclipboxnw("ClipBoxNwReset"); }
+
+        [CommandMethod("ClipBoxNw", CommandFlags.UsePickSet)]
+        public static void ClipBoxNw()
+        { doclipboxnw("ClipBoxNw"); }
+
+        public static void doclipboxnw(string command)
+        {
+            Helper.Initialize();
+
+            try
+
+            {
+
+                PromptSelectionResult selectionRes = Helper.oEditor.SelectImplied();
+
+                // If there's no pickfirst set available...
+
+                if (selectionRes.Status == PromptStatus.Error)
+
+                {
+
+                    // ... ask the user to select entities
+                    if (!command.Equals("ClipBoxNwReset"))
+                    {
+                        PromptSelectionOptions selectionOpts = new PromptSelectionOptions();
+
+                        selectionOpts.SingleOnly = true;
+                        selectionOpts.MessageForAdding = "\n(Unload NWD first, press esc now if still loaded) Select object to define area of interest..";
+
+                        if (command.Equals("ClipBoxNwWriteBatchOnly"))
+                        {
+                            selectionOpts.SingleOnly = false;
+                            selectionOpts.MessageForAdding = "\nSelect all objects defining areas and then press enter..";
+
+                        }
+                        selectionRes = Helper.oEditor.GetSelection(selectionOpts);
+                    }
+                }
+
+                else
+
+                {
+
+                    // If there was a pickfirst set, clear it
+
+                    Helper.oEditor.SetImpliedSelection(new ObjectId[0]);
+
+                }
+
+                // If the user has not cancellHelper.oEditor...
+
+                if (selectionRes.Status == PromptStatus.OK || command.Equals("ClipBoxNwReset"))
+
+                {
+
+                    // ... take the selected objects one by one
+
+
+
+
+                    using (Transaction tr = Helper.ActiveDocument.Database.TransactionManager.StartTransaction())
+                    {
+
+                        string documentspath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        string batpath = documentspath + "\\" + "clipboxnw.bat";
+                        string commandline = "";
+                        Point3d min = new Point3d(0, 0, 0);
+                        Point3d max = new Point3d(0, 0, 0);
+                        ObjectId[] objIds = new ObjectId[] { };
+
+                        if (!command.Equals("ClipBoxNwReset")) { objIds = selectionRes.Value.GetObjectIds(); }
+                        else objIds = new ObjectId[] { new ObjectId() };
+
+                        int counter = 0;
+
+                        foreach (ObjectId objId in objIds)
+                        {
+                            string suffix = "";
+                            if (counter > 0) suffix = "_" + counter.ToString();
+                            ++counter;                           
+
+                            if (!command.Equals("ClipBoxNwReset"))
+                            {
+                                Entity ent = (Entity)tr.GetObject(objId, OpenMode.ForRead);
+
+                                min = ent.Bounds.Value.MinPoint;
+                                max = ent.Bounds.Value.MaxPoint;
+
+                                ent.Dispose();
+                            }
+
+                            double conversionfactor = UnitsConverter.GetConversionFactor(Helper.oDatabase.Insunits, UnitsValue.Meters);
+
+                            commandline += "\"D:\\Program Files\\Autodesk\\Navisworks Manage 2020\\Roamer.exe\" -NoGui -OpenFile";
+                            commandline += " \"" + documentspath + "\\" + "CoordinationModel.nwd\"";
+                            commandline += " -ExecuteAddInPlugin \"ClipBox.AClipBox.ADSK\"";
+                            commandline += " \"" + documentspath + "\\" + "CoordinationModel" + suffix + ".nwd\"";
+                            commandline += " " + Math.Round(min.X * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += " " + Math.Round(min.Y * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += " " + Math.Round(min.Z * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += " " + Math.Round(max.X * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += " " + Math.Round(max.Y * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += " " + Math.Round(max.Z * conversionfactor, 3).ToString().Replace("-", "neg");
+                            commandline += "\r\n";
+                        }
+
+                        File.WriteAllText(batpath, commandline);
+
+
+                        if (!command.Equals("ClipBoxNwWriteBatchOnly"))
+                        {
+                            Process ExternalProcess = new Process();
+                            ExternalProcess.StartInfo.FileName = batpath;
+                            ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            ExternalProcess.Start();
+                            ExternalProcess.WaitForExit();
+                        }
+                        /*var btr = (BlockTableRecord)tr.GetObject(SymbolUtilityServices.GetBlockModelSpaceId(Helper.oDatabase), OpenMode.ForRead);
+
+                        foreach (ObjectId id in btr)
+                        {
+                            //Entity ente = (Entity)tr.GetObject(id, OpenMode.ForRead);
+                            DBObject ente = (DBObject)tr.GetObject(id, OpenMode.ForRead);
+                            Helper.oEditor.WriteMessage("\nmessage: " + ente.GetType().ToString());
+                            if (ente.GetType().ToString().EndsWith("ImpEntity"))
+                            {
+                                ((AcBIMUnderlayDbx)ente).Unload(false);
+                                //ShowType(id);
+                            }
+
+                        }*/
+                        string logtext = "";
+                        string logfilepath = documentspath + "\\" + "CoordinationModel.nwd.txt";
+                        if (File.Exists(logfilepath))
+                        {
+                            logtext = File.ReadAllText(logfilepath);
+                        }
+
+                        if (command.Equals("ClipBoxNwWriteBatchOnly"))
+                            Helper.oEditor.WriteMessage("\nclipboxnw.bat was created in the documents folder. Edit clipboxnw.bat for further use.");
+                        else {
+                            if (!logtext.Equals(""))
+                                Helper.oEditor.WriteMessage("\n" + logtext);
+                            else
+                                Helper.oEditor.WriteMessage("\nCoordinationModel.nwd was updated. Now reload CoordinationModel.nwd");
+                        }
+                        tr.Commit();
+                    }
+
+                }
+
+            }
+
+            catch (Autodesk.AutoCAD.Runtime.Exception ex)
+
+            {
+
+                Helper.oEditor.WriteMessage(ex.Message);
+
+            }
+
+            Helper.Terminate();
+        }
+
+        //https://adndevblog.typepad.com/autocad/2016/07/unload-all-the-pdf-underlay.html
+        //https://adndevblog.typepad.com/autocad/2016/11/how-to-retrieve-naviswork-model-nwd-or-bim-underlay-information-in-autocad-through-programming.html
+        public static void ShowType(ObjectId id) //unrelated method (test)
         {
             //Helper.Initialize();
             Document dwg = Helper.ActiveDocument;
@@ -54,141 +227,5 @@ namespace ClipBoxNw
 
             //Helper.Terminate();
         }
-
-        [CommandMethod("ClipBoxNwReset", CommandFlags.UsePickSet)]
-        public static void _ClipBoxNwReset()
-        { doclipboxnw(true); }
-
-        [CommandMethod("ClipBoxNw", CommandFlags.UsePickSet)]
-        public static void _ClipBoxNw()
-        { doclipboxnw(false); }
-
-        public static void doclipboxnw(bool reset)
-        {
-            Helper.Initialize();
-
-            try
-
-            {
-
-                PromptSelectionResult selectionRes =
-
-                  Helper.oEditor.SelectImplied();
-
-                // If there's no pickfirst set available...
-
-                if (selectionRes.Status == PromptStatus.Error && !reset)
-
-                {
-
-                    // ... ask the user to select entities
-
-                    PromptSelectionOptions selectionOpts =
-
-                      new PromptSelectionOptions();
-                    selectionOpts.SingleOnly = true;
-                    selectionOpts.MessageForAdding =
-
-                      "\n(Unload NWD first, press esc now if still loaded) Select object to define area of interest..";
-
-                    selectionRes =
-
-                      Helper.oEditor.GetSelection(selectionOpts);
-
-                }
-
-                else
-
-                {
-
-                    // If there was a pickfirst set, clear it
-
-                    Helper.oEditor.SetImpliedSelection(new ObjectId[0]);
-
-                }
-
-                // If the user has not cancellHelper.oEditor...
-
-                if (selectionRes.Status == PromptStatus.OK || reset)
-
-                {
-
-                    // ... take the selected objects one by one
-
-
-
-
-                    using (Transaction tr = Helper.ActiveDocument.Database.TransactionManager.StartTransaction())
-                    {
-                        string documentspath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                        string batpath = documentspath + "\\" + "clipboxnw.bat";
-                        Point3d min = new Point3d();
-                        Point3d max = new Point3d();
-
-                        if (!reset)
-                        {
-                            ObjectId objId = selectionRes.Value.GetObjectIds()[0];
-
-                            Entity ent = (Entity)tr.GetObject(objId, OpenMode.ForRead);
-
-                            min = ent.Bounds.Value.MinPoint;
-                            max = ent.Bounds.Value.MaxPoint;
-
-                            ent.Dispose();
-                        }
-
-                        string commandline = "\"D:\\Program Files\\Autodesk\\Navisworks Manage 2020\\Roamer.exe\" -NoGui -OpenFile";
-                        commandline += " \"" + documentspath + "\\" + "CoordinationModel.nwd\"";
-                        commandline += " -ExecuteAddInPlugin \"ClipBox.AClipBox.ADSK\"";
-                        commandline += " \"" + documentspath + "\\" + "CoordinationModel.nwd\"";
-                        commandline += " " + Math.Round(min.X / 1000, 3).ToString().Replace("-", "neg");
-                        commandline += " " + Math.Round(min.Y / 1000, 3).ToString().Replace("-", "neg");
-                        commandline += " " + Math.Round(min.Z / 1000, 3).ToString().Replace("-", "neg");
-                        commandline += " " + Math.Round(max.X / 1000, 3).ToString().Replace("-", "neg");
-                        commandline += " " + Math.Round(max.Y / 1000, 3).ToString().Replace("-", "neg");
-                        commandline += " " + Math.Round(max.Z / 1000, 3).ToString().Replace("-", "neg");
-
-                        File.WriteAllText(batpath, commandline);
-
-                        Process ExternalProcess = new Process();
-                        ExternalProcess.StartInfo.FileName = batpath;
-                        ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                        ExternalProcess.Start();
-                        ExternalProcess.WaitForExit();
-
-                        /*var btr = (BlockTableRecord)tr.GetObject(SymbolUtilityServices.GetBlockModelSpaceId(Helper.oDatabase), OpenMode.ForRead);
-
-                        foreach (ObjectId id in btr)
-                        {
-                            //Entity ente = (Entity)tr.GetObject(id, OpenMode.ForRead);
-                            DBObject ente = (DBObject)tr.GetObject(id, OpenMode.ForRead);
-                            Helper.oEditor.WriteMessage("\nmessage: " + ente.GetType().ToString());
-                            if (ente.GetType().ToString().EndsWith("ImpEntity"))
-                            {
-                                ((AcBIMUnderlayDbx)ente).Unload(false);
-                                //ShowType(id);
-                            }
-
-                        }*/
-
-                        Helper.oEditor.WriteMessage("\nCoordinationModel.nwd was updated! Now reload CoordinationModel.nwd!");
-                        tr.Commit();
-                    }
-
-                }
-
-            }
-
-            catch (Autodesk.AutoCAD.Runtime.Exception ex)
-
-            {
-
-                Helper.oEditor.WriteMessage(ex.Message);
-
-            }
-
-            Helper.Terminate();
-        }
-
     }
 }
